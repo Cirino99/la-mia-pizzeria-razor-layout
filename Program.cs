c298e84743d4334be8adf62e719d5f94ce2d1fa6
Simone <cirino.simone99@gmail.com>
2022-11-18 14:23:11 +0100

    popolata vista snacks

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();  //autoload
 
 var app = builder.Build();
 
